08d4cb9eb15ab7a6eb386e09df25ac713dfefec2
sethr07 <sethr@tcd.ie>
2023-06-18 17:37:29 +0100

    Harsh STuff.

--- make.sh
+++ make.sh
@@ -4,16 +4,14 @@ src_dir="src"
 bin_dir="bin"
 
 # CVs
-del_cv="cv_del.tex"
 dub_cv="cv.tex"
 
 # Cover Letters
-del_cl="cl.tex"
+dub_cl="cl.tex"
 
 cd $src_dir
-pdflatex $del_cv 
 pdflatex $dub_cv
-pdflatex $del_cl
+pdflatex $dub_cl
 
 mv *.pdf ../$bin_dir
 
